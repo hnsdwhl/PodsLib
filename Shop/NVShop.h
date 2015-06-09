a5a11eb8eeb091f4eb3a156bd0cd3030366866ba
hualin.wang <hualin.wang@dianping.com>
2015-06-09 14:31:51 +0800

    add shop model

---
+++ Shop/NVShop.h
@@ -0,0 +1,15 @@
+//
+//  NVShop.h
+//  ScrollingNavbarDemo
+//
+//  Created by Wang Hualin on 15/6/9.
+//  Copyright (c) 2015年 Andrea Mazzini. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NVShop : NSObject
+@property (nonatomic, assign) NSInteger shopId;
+@property (nonatomic, copy) NSString *shopName;
+
+@end
---
+++ Shop/NVShop.m
@@ -0,0 +1,13 @@
+//
+//  NVShop.m
+//  ScrollingNavbarDemo
+//
+//  Created by Wang Hualin on 15/6/9.
+//  Copyright (c) 2015年 Andrea Mazzini. All rights reserved.
+//
+
+#import "NVShop.h"
+
+@implementation NVShop
+
+@end
